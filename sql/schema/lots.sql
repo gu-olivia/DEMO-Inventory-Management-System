cd6d9724e27dd1952c36a6a17013547f9f36b2d1
gu-olivia <gu.olivia.r@gmail.com>
2026-07-14 14:47:40 -0400

    Update lots.sql

--- sql/schema/lots.sql
+++ sql/schema/lots.sql
@@ -1,1 +1,18 @@
+-- Describes attributes that are specific to one lot.
+-- Bulk and packaged specifications are stored in their respective tables.
+CREATE TABLE lots (
+    lot_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    product_id INT NOT NULL,
+    clone_name VARCHAR(50),
+    lot_number VARCHAR(50),
+    creation_date DATE,
+    expiration_date DATE,
+    product_state VARCHAR(50), -- e.g. Active or DQ'd
+    test_5uL_concentration_ug_mL DECIMAL(10,2) NOT NULL,
+    initial_volume_mL DECIMAL(10,2) NOT NULL,
+    remarks VARCHAR(50),
 
+    CONSTRAINT fk_lot_product
+        FOREIGN KEY (product_id)
+        REFERENCES products(product_id)
+);
