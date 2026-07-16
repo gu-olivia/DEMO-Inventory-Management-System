a3b5345c3b51ed50cd1e965adb0b107024827042
Olivia <duanolivia@gmail.com>
2026-07-15 22:10:32 -0400

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,30 +1,20 @@
 from sqlalchemy import create_engine
-from sqlalchemy import text
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
-from urllib.parse import quote_plus
-import os
+from sqlalchemy.orm import declarative_base, sessionmaker
 from dotenv import load_dotenv
-
-
-Base = declarative_base()
-
-connection_string = (
-    "Trusted_Connection=yes;"
-    "TrustServerCertificate=yes;"
-)
+import os
 
 load_dotenv()
+
+Base = declarative_base()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    pool_pre_ping=True
 )
+
 SessionLocal = sessionmaker(bind=engine)
-with engine.connect() as conn:
-    conn.execute(text("SELECT 1"))
 
 def get_db():
     db = SessionLocal()
