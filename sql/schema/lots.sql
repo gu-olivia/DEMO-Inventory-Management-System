cf1c04a2777732d4bb3de4f9694e0a1b71663a03
gu-olivia <gu.olivia.r@gmail.com>
2026-07-23 23:12:51 -0400

    Update lots.sql

--- sql/schema/lots.sql
+++ sql/schema/lots.sql
@@ -9,7 +9,7 @@ CREATE TABLE lots (
     expiration_date DATE,
     product_state VARCHAR(50), -- e.g. Active or DQ'd
     test_5ul_concentration_ug_ml DECIMAL(10,2) NOT NULL,
-    initial_volume_mL DECIMAL(10,2) NOT NULL,
+    initial_volume_ml DECIMAL(10,2) NOT NULL,
     remarks VARCHAR(50),
 
     CONSTRAINT fk_lot_product
